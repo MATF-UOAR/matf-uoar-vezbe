5a7061375222bf2fca333d53d51ebc958847191b
ognjenivuk <ognjen7amg@gmail.com>
2026-05-11 11:48:35 +0200

    Use fp and lr aliases in ARM64 examples

--- 07-uvod-arm64/01-sabiranje/saberi-sa-stek-okvirom.s
+++ 07-uvod-arm64/01-sabiranje/saberi-sa-stek-okvirom.s
@@ -6,15 +6,18 @@
 // int saberi(int a, int b)
 // w0          w0     w1
 saberi:
-    stp x29, x30, [sp, -32]!
-    mov x29, sp
+    // fp and lr are aliases for x29 and x30; either spelling works.
+    stp fp, lr, [sp, #-16]!
+    mov fp, sp
+    sub sp, sp, #16
 
-    str w0, [sp, 24]
-    str w1, [sp, 28]
+    str w0, [sp, 8]
+    str w1, [sp, 12]
 
-    ldr w2, [sp, 24]
-    ldr w3, [sp, 28]
+    ldr w2, [sp, 8]
+    ldr w3, [sp, 12]
     add w0, w2, w3
 
-    ldp x29, x30, [sp], 32
+    mov sp, fp
+    ldp fp, lr, [sp], #16
     ret
--- 07-uvod-arm64/02-hello-world/hello.s
+++ 07-uvod-arm64/02-hello-world/hello.s
@@ -8,13 +8,13 @@ msg: .asciz "Hello world!"
 .global main
 
 main:
-    stp x29, x30, [sp, -16]!
-    mov x29, sp
+    stp fp, lr, [sp, #-16]!
+    mov fp, sp
 
     adr x0, msg
     bl puts
 
     mov w0, #0
 
-    ldp x29, x30, [sp], 16
+    ldp fp, lr, [sp], #16
     ret
