1ad8f03f362b00b16ebed06920a75af73ceac1d3
ognjenivuk <ognjen7amg@gmail.com>
2026-06-09 22:29:29 +0200

    resenja drugog primera

--- 12-vezbanje-primeri-ispita/02-primer/resenja/2.s
+++ 12-vezbanje-primeri-ispita/02-primer/resenja/2.s
@@ -1,4 +1,51 @@
 .intel_syntax noprefix
 .section .note.GNU-stack, "", @progbits
 
+.text
+
+.global najtezi
+
 #  unsigned long najtezi(unsigned long *a, int n);
+#      rax                     rdi          esi
+najtezi:
+	enter 0, 0
+
+	mov rdx, 0   # rdx - max_weight
+	mov r8, 0    # r8 - result
+
+	mov ecx, 0   # ecx - i
+for_loop:
+	cmp ecx, esi
+	jge for_loop_end
+
+		push rdi
+		push rsi
+		push rdx
+		push r8
+		push rcx
+		push rcx
+
+		mov rdi, [rdi + 8 * rcx]
+		call tezina
+
+		pop rcx
+		pop rcx
+		pop r8
+		pop rdx
+		pop rsi
+		pop rdi
+
+		cmp rax, rdx
+		jbe dalje
+			mov rdx, rax
+			mov r8, [rdi + 8 * rcx]
+	dalje:
+
+	inc ecx
+	jmp for_loop
+for_loop_end:
+
+	mov rax, r8
+
+	leave
+	ret
--- 12-vezbanje-primeri-ispita/02-primer/resenja/4.s
+++ 12-vezbanje-primeri-ispita/02-primer/resenja/4.s
@@ -1,1 +1,47 @@
+.text
+
+.align 2
+
+.global broj_tipicnih
+
 // int broj_tipicnih(unsigned long *a, int n);
+// w0                    x0            w1
+broj_tipicnih:
+	stp fp, lr, [sp, #-16]!
+	mov fp, sp
+	sub sp, sp, #32
+
+	// -------------------- <- sp = fp - 32
+	//
+	//  prostor za median
+	// -------------------- <- fp - 24
+    //
+	//  prostor za mean
+	// -------------------- <- fp - 16
+	//  poravnanje adresa
+	// -------------------- <- fp - 12
+	//  prostor za n
+	// -------------------- <- fp - 8
+	// 
+	//  prostor za a 
+	// -------------------- <- fp
+	str x0, [fp, #-8]
+	str w1, [fp, #-12]
+	sub x2, fp, #24  // adresa mean
+	sub x3, fp, #32  // adresa median
+	bl statistika    // popunjava memoriju na steku (prosledjenu kao 2. i 3. arg)
+
+	ldr x0, [fp, #-8]
+	ldr w1, [fp, #-12]
+	ldr x4, [fp, #-24]  // x4 - mean
+	ldr x5, [fp, #-32]  // x5 - median
+	cmp x4, x5          // poredimo mean i median
+	csel x2, x4, x5, lo // manji ide u x2
+	csel x3, x4, x5, hi // veci ide u x3
+	
+	bl broj_u_opsegu
+
+
+	mov sp, fp
+	ldp fp, lr, [sp], #16
+	ret
